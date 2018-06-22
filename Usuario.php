687693b82b4092e05b87c0f5f20fc1d596f5660c
Francisco1985Cuevas <FCuevas@VOICENTER.COM.PY>
2018-06-22 15:54:52 -0400

    commit del 22-06-2018, Agregado clase usuario en PHP

--- Usuario.php
+++ Usuario.php
@@ -1,3 +1,42 @@
 <?php
-	echo "HOLA MUNDO";
+	//echo "HOLA MUNDO";
+	class Usuario{
+		// propiedades o atributos
+		private $nombre;
+		private $codigo;
+		private $telefono;
+		
+		// constructor
+		public __construct($nombre, $codigo, $telefono){
+			$this->nombre = $nombre;
+			$this->codigo = $codigo;
+			$this->telefono = $telefono;
+		}
+		
+		//metodos get
+		public getNombre(){
+			return $this->nombre;
+		}
+		
+		public getCodigo(){
+			return $this->codigo;
+		}
+		
+		public getTelefono(){
+			return $this->telefono;
+		}
+		
+		// metodos set
+		public setNombre( $nom ){
+			$this->nombre = $nom;
+		}
+		
+		public setCodigo( $cod ) {
+			$this->codigo = $cod;
+		}
+		
+		public setTelefono( $tel ) {
+			$this->telefono = $tel;
+		}
+	}
 ?>
